60a417b690d2a49d868197a667ff4a9010d74395
Diego Cerqueira <xdiegocerqueira@gmail.com>
2021-10-10 09:35:01 -0300

    adding json config

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)  # Flask core instance initiated
 app.config["SQLALCHEMY_DATABASE_URI"] = s.os.environ.get("DATABASE_URL")
+app.config['JSON_SORT_KEYS'] = False
 
 from app import routes
