bc144faafc6bf97c6ae4578c3f59fa70010f0bc6
Diego Cerqueira <xdiegocerqueira@gmail.com>
2021-10-10 09:42:22 -0300

    retrieving user timeline and profile details

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 from app.models import Analises, AnaliseSchema
+from app.tp import api, getUserTimeline, getDataFromUserByTwiiterHandle as findByHandle
 
-@app.get("/botometer/<handle>")
+@app.get("/")
+@app.get("/botometer")
+def botometer():
+    handle = request.args.get('profile')
+    user = findByHandle(handle)
+    timeline = getUserTimeline(user.twitter_id_str, num_tweets=5)
+    return jsonify({'user': user, 'timeline':timeline})
+    # user = Analises.query.filter_by(handle=handle).first()
+    # user = Analises.query.get(1)
+    # print(f'{user}')
+    analise_schema = AnaliseSchema()
+    return jsonify(analise_schema.dump(user))
+
 def botometer(handle):
     user = Analises.query.filter_by(handle=handle).first()
     # user = Analises.query.get(1)
