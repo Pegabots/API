4ca7cfe74442feb0101a8e35d54312999f61d2f5
Diego Cerqueira <xdiegocerqueira@gmail.com>
2021-10-19 14:52:55 -0300

    adding poetry dependency management to the project

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-source venv/bin/activate
+source .venv/bin/activate
 export FLASK_APP=./app/api.py
 export FLASK_ENV=development
-pip install -r ./requirements.txt
+poetry install
 flask run
