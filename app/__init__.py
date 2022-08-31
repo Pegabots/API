5c2c2153859aab5dcf734b2193274117619cd6fb
Alan Veloso <alantsv@gmail.com>
2022-08-31 15:55:31 -0300

    Added config to sql track modifications

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ from flask import Flask
 app = Flask(__name__)  # Flask core instance initiated
 app.config["SQLALCHEMY_DATABASE_URI"] = s.os.environ.get("DATABASE_URL")
 app.config['JSON_SORT_KEYS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app.routes import routes
