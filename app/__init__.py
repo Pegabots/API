a342e98aa9c320cd6a643cbbfbba12d07150d8e7
williamneto <william.g.neto@gmail.com>
2022-09-03 22:21:21 -0300

    Estrutura de autenticação

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 import settings as s
 from flask import Flask
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder="templates/")  # Flask core instance initiated
 app.config["SQLALCHEMY_DATABASE_URI"] = s.os.environ.get("DATABASE_URL")
 app.config['JSON_SORT_KEYS'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] ="LOINPV0898h-987hoIUB086vgo(*¨F(&tvOUYVhygvioutgf97VUvg"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from app.routes.analise.route import router as analise_router
 from app.routes.auth.route import router as auth_router
+from app.routes.lab.route import router as lab_router
 
 app.register_blueprint(analise_router)
 app.register_blueprint(auth_router)
+app.register_blueprint(lab_router)
--- app/routes/auth/route.py
+++ app/routes/auth/route.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask import jsonify, request
+from flask_login import current_user, login_user
 
-from app.models.models import Analises, AnaliseSchema
-from app.services.botometer_service import BotometerService
+from app import login_manager
+from app.routes.auth.forms import SignupForm, LoginForm
+from app.models.auth import User
+from app.models import db
 
 router = Blueprint(
     "auth",
@@ -11,14 +14,68 @@ router = Blueprint(
     template_folder="templates"
 )
 
-@router.get("/login")
+@router.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Log-in page for registered users.
+
+    GET requests serve Log-in page.
+    POST requests validate and redirect user to dashboard.
+    """
+    # Bypass if user is logged in
+    if current_user.is_authenticated:
+        return redirect(url_for("lab.home"))
+
+    form = LoginForm()
+    # Validate login attempt
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(password=form.password.data):
+            login_user(user)
+            next_page = request.args.get("next")
+            return redirect(next_page or url_for("lab.home"))
+        flash("Invalid username/password combination")
+        return redirect(url_for("auth.login"))
     return render_template(
         "login.html",
+        form=form,
+        title="Log in.",
+        template="login-page",
+        body="Log in with your User account.",
     )
 
-@router.get("/register")
-def register():
+@login_manager.user_loader
+def load_user(user_id):
+    """Check if user is logged-in upon page load."""
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+@router.route("/signup", methods=["GET", "POST"])
+def signup():
+    """
+    User sign-up page.
+
+    GET requests serve sign-up page.
+    POST requests validate form & user creation.
+    """
+    form = SignupForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        if existing_user is None:
+            user = User(
+                name=form.name.data, email=form.email.data
+            )
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()  # Create new user
+            
+            return redirect(url_for("auth.login"))
+        flash("A user already exists with that email address.")
     return render_template(
-        "register.html"
+        "signup.html",
+        title="Create an Account.",
+        form=form,
+        template="signup-page",
+        body="Sign up for a user account.",
     )
